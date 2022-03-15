a1284e05ffa3a296f9bcf47a1c28f9753d2706a7
Eivind Mork <eivind.mork@gyldendal.no>
2022-03-15 08:43:58 +0100

    Update template handler to work with Rails 6.1

--- lib/liquid-rails/template_handler.rb
+++ lib/liquid-rails/template_handler.rb
@@ -2,7 +2,7 @@ module Liquid
   module Rails
     class TemplateHandler
 
-      def self.call(template)
+      def self.call(template, source)
         "Liquid::Rails::TemplateHandler.new(self).render(#{template.source.inspect}, local_assigns)"
       end
 
